2145a9400e76b4fde3b58af8a6fb0d0ef4d25388
junwoo <junwookapa@naver.com>
2016-03-23 00:13:24 +0900

    first commit

--- src/main/java/com/jun/framework/App.java
+++ src/main/java/com/jun/framework/App.java
@@ -4,13 +4,20 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.journaldev.di.consumer.MyApplication;
 import com.journaldev.di.injector.AppInjector;
+import static spark.Spark.get;
 
 public class App {
 	public static void main(String[] args) {
 		Injector injector = Guice.createInjector(new AppInjector());
-		
 		MyApplication app = injector.getInstance(MyApplication.class);
 		app.sendMessage("Hi Pankaj", "pankaj@abc.com");
+		endPoint();
+	}
+
+	public static void endPoint() {
+		get("/", "application/json", (request, response) -> {
+			return "hello world";
+		});
 	}
 
 }
